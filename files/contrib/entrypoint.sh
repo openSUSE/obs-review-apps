bb961f5bd64396b9b98f1512bb5041e460ae21de
Christian Bruckmayer <cbruckmayer@suse.com>
2018-08-14 10:37:09 +0200

    Use sudo to install bundle

--- files/contrib/entrypoint.sh
+++ files/contrib/entrypoint.sh
@@ -2,7 +2,7 @@
 echo "Preparing application..."
 rm -rf tmp/pids/server.pid
 rm -rf log/development.sphinx.pid
-bundle install
+sudo bundle install
 RAILS_ENV=development bundle exec rake dev:bootstrap
 RAILS_ENV=development bundle exec rake assets:precompile
 RAILS_ENV=development bundle exec rake dev:development_testdata:create
